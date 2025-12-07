6d4d629999820f9efdb07582c505d8bbaf68303a
KaueNoites <kauenoites@outlook.com>
2025-12-07 14:21:38 -0300

    Revert "fix: correct streak, best/worst day calculations and fix charts in stats screen"
    This reverts commit c4494244136e469bedf0710a25129032e1800f8b.

--- api/stat.tsx
+++ api/stat.tsx
@@ -1,6 +1,4 @@
-
-import { HabitFrequency } from "@/lib/types";
-import api from "./config/axios";
+import { HabitFrequency, Habit, HabitCompletion } from "@/lib/types";
 
 export async function getHabitStats(
   frequency?: HabitFrequency,
@@ -12,68 +10,11 @@ export async function getHabitStats(
   completed: number;
   pending: number;
   notCompleted: number;
-  longestStreak?: number;
 }> {
-  try {
-
-    const params: any = {};
-    
-    if (frequency) {
-      params.frequency = frequency;
-    }
-    
-    if (startDate) {
-      params.startDate = startDate;
-    }
-    
-    if (endDate) {
-      params.endDate = endDate;
-    }
-    
-    if (tags && tags.length > 0) {
-      params.tags = tags.join(',');
-    }
-    
-    const response = await api.get('/stats', { params });
-    const habitsStats = response.data.data;
-  
-    let totalHabits = habitsStats.length;
-    let completedHabits = 0;
-    let pendingHabits = 0;
-    let notCompletedHabits = 0;
-  
-    habitsStats.forEach((habitStat: any) => {
-      if (habitStat.positive >= habitStat.expected) {
-        completedHabits++;
-      } 
-
-      else if (habitStat.negative > 0) {
-        notCompletedHabits++;
-      }
-
-      else {
-        pendingHabits++;
-      }
-    });
-    const longestStreak = 0;
-    
-    return {
-      total: totalHabits,
-      completed: completedHabits,
-      pending: pendingHabits,
-      notCompleted: notCompletedHabits,
-      longestStreak,
-    };
-    
-  } catch (error) {
-    console.error("Erro ao buscar estatísticas:", error);
-
-    return {
-      total: 0,
-      completed: 0,
-      pending: 0,
-      notCompleted: 0,
-      longestStreak: 0,
-    };
-  }
+  return {
+    total: 1,
+    completed: 1,
+    pending: 1,
+    notCompleted: 1,
+  };
 }
